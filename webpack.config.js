6442f7935c45f828db156b16a5e8db567cc6ff56
Michał Terczyński <mterczynski271@gmail.com>
2018-04-20 22:15:23 +0200

    Updated to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,7 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
   },
   module: {
-    loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+    rules:[
       { test: /\.tsx?$/, loader: 'ts-loader' }
     ]
   }

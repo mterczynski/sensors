5e2956c09da009cf8b21741534df39136b39a81e
Michal Terczynski <mterczynski271@gmail.com>
2019-12-06 15:30:24 +0100

    changed block color to orange

--- src/app.ts
+++ src/app.ts
@@ -165,7 +165,7 @@ export class App {
 
   drawObstacles() {
     this.levelData.forEach((tile) => {
-      this.ctx.fillStyle = 'rgb(0, 200, 0)';
+      this.ctx.fillStyle = 'rgb(200, 100, 42)';
       this.ctx.fillRect(tile.x * tileSize, tile.z * tileSize, tileSize, tileSize);
     });
   }

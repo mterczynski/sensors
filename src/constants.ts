d9cb9f9f12b974621786d23f98fd79bd78b64570
Michal Terczynski <mterczynski271@gmail.com>
2020-06-15 12:35:11 +0200

    Code styling

--- src/constants.ts
+++ src/constants.ts
@@ -11,8 +11,8 @@ export const tileSize = 40;
 export const wallColor = 'rgb(0, 160, 120)';
 export const sensorLineColor = 'rgb(200, 0, 0)';
 export const pointOfCollisionColor = 'rgb(255, 0, 0)';
-export const canvasBackgroundColor = 'rgb(240,240,240)';
+export const canvasBackgroundColor = 'rgb(240, 240, 240)';
 export const gridLineColor = 'rgb(200, 200, 200)';
 
-export const aliveBotColor = 'rgb(100,100,255)';
+export const aliveBotColor = 'rgb(100, 100, 255)';
 export const deadBotColor = 'rgb(100, 100, 100)';

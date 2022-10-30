f5a18a42bb1238c89d202ea442d906a6a397de97
Michał Terczyński <mterczynski1@gmail.com>
2022-10-31 00:56:33 +0100

    Add a todo-like comment

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,6 @@
-import { level01 } from './level-data';
+import { level01 } from "./level-data";
 
-export const startingBotPosition = {x: 3, y: 8};
+export const startingBotPosition = { x: 3, y: 8 }; // this one should be moved to level data
 export const sensorsPerBotCount = 5;
 export const populationSize = 5;
 export const activeLevel = level01;
@@ -10,11 +10,11 @@ export const activeLevel = level01;
 export const pointOfCollisionRadius = 5;
 export const tileSize = 40;
 
-export const wallColor = 'rgb(0, 160, 120)';
-export const sensorLineColor = 'rgb(200, 0, 0)';
-export const pointOfCollisionColor = 'rgb(255, 0, 0)';
-export const canvasBackgroundColor = 'rgb(240, 240, 240)';
-export const gridLineColor = 'rgb(200, 200, 200)';
+export const wallColor = "rgb(0, 160, 120)";
+export const sensorLineColor = "rgb(200, 0, 0)";
+export const pointOfCollisionColor = "rgb(255, 0, 0)";
+export const canvasBackgroundColor = "rgb(240, 240, 240)";
+export const gridLineColor = "rgb(200, 200, 200)";
 
-export const aliveBotColor = 'rgb(100, 100, 255)';
-export const deadBotColor = 'rgb(100, 100, 100)';
+export const aliveBotColor = "rgb(100, 100, 255)";
+export const deadBotColor = "rgb(100, 100, 100)";

ce8b6c21e98d2011b7a85558c09814167840fe17
Michał Terczyński <mterczynski1@gmail.com>
2024-01-15 16:25:00 +0100

    Add level 3 and 4 (easier versions of level 2)

---
+++ src/level-data/level-3.ts
@@ -0,0 +1,988 @@
+// tslint:disable:object-literal-sort-keys
+
+// a bit easier than level02
+export const level03 = {
+  size: 19,
+  tiles: [
+    {
+      x: 0,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 1,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 2,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 3,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 8,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 11,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 16,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 17,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 0,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 1,
+      type: 'wall',
+    },
+    {
+      x: 1,
+      y: 1,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 1,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 1,
+      type: 'wall',
+    },
+    {
+      x: 11,
+      y: 1,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 1,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 2,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 2,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 2,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 2,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 3,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 3,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 3,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 3,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 4,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 4,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 4,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 4,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 4,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 4,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 4,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 4,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 5,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 5,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 5,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 5,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 5,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 5,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 5,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 5,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 5,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 5,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 6,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 6,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 6,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 6,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 6,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 6,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 6,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 6,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 6,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 6,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 8,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 8,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 11,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 11,
+      y: 7,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 8,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 8,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 11,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 9,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 8,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 11,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 10,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 8,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 11,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 11,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 8,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 11,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 12,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 8,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 11,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 13,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 14,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 14,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 15,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 15,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 15,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 15,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 16,
+      type: 'wall',
+    },
+    {
+      x: 1,
+      y: 16,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 16,
+      type: 'wall',
+    },
+    {
+      x: 17,
+      y: 16,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 16,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 17,
+      type: 'wall',
+    },
+    {
+      x: 1,
+      y: 17,
+      type: 'wall',
+    },
+    {
+      x: 2,
+      y: 17,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 17,
+      type: 'wall',
+    },
+    {
+      x: 16,
+      y: 17,
+      type: 'wall',
+    },
+    {
+      x: 17,
+      y: 17,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 17,
+      type: 'wall',
+    },
+    {
+      x: 0,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 1,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 2,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 3,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 4,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 5,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 6,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 7,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 8,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 9,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 10,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 11,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 12,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 13,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 14,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 15,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 16,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 17,
+      y: 18,
+      type: 'wall',
+    },
+    {
+      x: 18,
+      y: 18,
+      type: 'wall',
+    },
+  ],
+};

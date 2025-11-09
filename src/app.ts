fe16b7f97c718a8f02e0b1701f0ba9f2f1f5ca0d
Michał Terczyński <mterczynski1@gmail.com>
2025-11-09 18:09:17 +0100

    Crate public init method in app.ts

--- src/app.ts
+++ src/app.ts
@@ -72,7 +72,9 @@ export class App {
     }
   }
 
-  constructor() {
+  constructor() {}
+
+  init() {
     requestAnimationFrame(() => this.onNextAnimationFrame());
     this.stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
     document.body.appendChild(this.stats.dom);
